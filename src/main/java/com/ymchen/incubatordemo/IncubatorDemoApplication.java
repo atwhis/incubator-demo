6bbf4130ad5ef4e5dbe36009c9c8aa0584e77ff1
ymchen <ymchen@demo.com>
2022-07-28 17:49:21 +0800

    incubator-demo init

---
+++ src/main/java/com/ymchen/incubatordemo/IncubatorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ymchen.incubatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubatorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncubatorDemoApplication.class, args);
+    }
+
+}
