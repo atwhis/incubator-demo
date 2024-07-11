7dffacd9c86916e9f9d0d85ed926e8da3d609d15
ymchen <ymchen@demo.com>
2024-07-11 11:23:57 +0800

    remove test demo

--- src/main/java/com/ymchen/incubatordemo/IncubatorDemoApplication.java
+++ src/main/java/com/ymchen/incubatordemo/IncubatorDemoApplication.java
@@ -1,10 +1,8 @@
 package com.ymchen.incubatordemo;
 
-import com.ymchen.external.MovieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -15,15 +13,16 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class IncubatorDemoApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(IncubatorDemoApplication.class, args);
-//        TestService testService = configurableApplicationContext.getBean("testService", TestService.class);
-//        log.info(testService.testEnhancer("zhangSan"));
-//        log.info(testService.testEnhancer2("zhangSan2"));
-//        configurableApplicationContext.close();
-
-        // test spring default scan path
-        MovieService movieService = configurableApplicationContext.getBean("movieService", MovieService.class);
-        movieService.getMovieInfo();
+//        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(IncubatorDemoApplication.class, args);
+////        TestService testService = configurableApplicationContext.getBean("testService", TestService.class);
+////        log.info(testService.testEnhancer("zhangSan"));
+////        log.info(testService.testEnhancer2("zhangSan2"));
+////        configurableApplicationContext.close();
+//
+//        // test spring default scan path
+//        MovieService movieService = configurableApplicationContext.getBean("movieService", MovieService.class);
+//        movieService.getMovieInfo();
+        SpringApplication.run(IncubatorDemoApplication.class, args);
     }
 
 }
